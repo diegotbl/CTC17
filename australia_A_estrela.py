b3dc1762a2e2680da3c582db1fa5db430da0408d
Unknown <moreno6230@gmail.com>
2019-08-27 21:39:39 -0300

    finish Ex1

--- australia_A_estrela.py
+++ australia_A_estrela.py
@@ -1,4 +1,5 @@
 import copy
+from prettytable import PrettyTable
 
 class City:
     def __init__(self, ident, name, x, y, pai, h, g, f, expand ):
@@ -16,7 +17,6 @@ class City:
 def find_city(all_cities, city):
     return next(x for x in all_cities if x.name == city)
 
-# Funcao que ira listar os vizinhos de cada cidade
 def verify_neighbors(all_cities):
     """
     Evaluates neighbors of each city
@@ -46,10 +46,10 @@ def verify_neighbors(all_cities):
 #FUNCAO PARA INSERIR AS CIDADES NA VIZINHANCA E CHAMAR CALCULO DE G(N) E H(N)
 def insert_neighbors(cities, all_cities, neighbors, actual_city, destiny):
     #APAGAR
-    print("FUNCAO EXPANSAO")
-    print(actual_city,"name: ", cities[actual_city].name, "id:",  int(cities[actual_city].ident))
+    # print("FUNCAO EXPANSAO")
+    # print(actual_city,"name: ", cities[actual_city].name, "id:",  int(cities[actual_city].ident))
     all_cities[int(cities[actual_city].ident)-1].expand = 1
-    print("bloqueando", all_cities[int(cities[actual_city].ident)-1].name)
+    # print("bloqueando", all_cities[int(cities[actual_city].ident)-1].name)
     #para todos os vizinhos da cidade atual
     for i in neighbors[int(cities[actual_city].ident)-1]:
         a = copy.deepcopy(all_cities[i-1])
@@ -58,13 +58,10 @@ def insert_neighbors(cities, all_cities, neighbors, actual_city, destiny):
         #calculo da distancia em linha reta da cidade em questao ate o destino
         a.h = h_distance(a, destiny[0])
         a.f = a.g + a.h
-        a.pai = int(cities[actual_city].ident)
+        a.pai = actual_city
         cities.append(City(a.ident, a.name, a.x, a.y, a.pai, a.h, a.g, a.f, 0))
         #APAGAR
-        print("id:", a.ident,"name" "{0:<20}".format(a.name),"f", a.f,"g", a.g,"h", a.h)
-
-
-
+        # print("id:", a.ident,"name" "{0:<20}".format(a.name),"f", a.f,"g", a.g,"h", a.h)
 
 # Funcao para calcular a distancia g
 def g_distance (a,b):
@@ -73,6 +70,78 @@ def g_distance (a,b):
 def h_distance (a,b):
     return ((float(a.x) - float(b.x))**2 + (float(a.y) - float(b.y))**2)**0.5
 
+def a_estrela (cities, all_cities, neighbors, actual_city, destiny):
+        #marcar cidade como expandida
+        #print("-----------------------------------------------------")
+        cities[actual_city].expand = 1
+
+        #APAGAR
+        #print("\nexpandindo ", cities[actual_city].name, "expand", cities[actual_city].expand)
+
+        # Inserir todos os vizinhos da actual_city no vetor cities
+        insert_neighbors(cities, all_cities, neighbors, actual_city, destiny)
+        n = len(cities)
+
+
+        aux = actual_city
+        for j in range(n):
+            if cities[j].expand == 0:
+                if (cities[j].f < cities[aux].f or cities[aux].expand == 1) and cities[j].ident != cities[0].ident:
+                    if all_cities[int(cities[j].ident)-1].expand == 0:
+                        aux = j
+        actual_city = aux
+
+        return actual_city
+
+
+def greedy(cities, all_cities, neighbors, actual_city, destiny):
+    # marcar cidade como expandida
+    # print("-----------------------------------------------------")
+    cities[actual_city].expand = 1
+
+    # APAGAR
+    # print("\nexpandindo ", cities[actual_city].name, "expand", cities[actual_city].expand)
+
+    # Inserir todos os vizinhos da actual_city no vetor cities
+    insert_neighbors(cities, all_cities, neighbors, actual_city, destiny)
+    n = len(cities)
+
+    aux = actual_city
+    for j in range(n):
+        if cities[j].expand == 0:
+            if (cities[j].h < cities[aux].h or cities[aux].expand == 1) and cities[j].ident != cities[0].ident:
+                if all_cities[int(cities[j].ident) - 1].expand == 0:
+                    aux = j
+
+    actual_city = aux
+    # for i in cities:
+    #     print("i:", i.ident, "name:" "{0:<20}".format(i.name),"expand:", i.expand, "h:", i.h)
+    # print(actual_city, "name", cities[actual_city].name, "f", cities[actual_city].f)
+
+
+    return actual_city
+def find_route (cities, actual_city):
+    i = cities[actual_city]
+    #print("i:", i.ident, "name:" "{0:<20}".format(i.name), "expand:", i.expand, "pai:", i.pai)
+    route = []
+    route.append(i)
+    while i.pai != -1:
+        i = cities[i.pai]
+        route.append(i)
+        #print("i:", i.ident, "name:" "{0:<20}".format(i.name), "expand:", i.expand, "pai:", i.pai, cities[i.pai].ident, cities[i.pai].name)
+        #print(i.name, i.ident)
+    return route
+
+def print_route (route, nome):
+    route.reverse()
+    traject = open(nome, "w")
+    x = PrettyTable()
+    x.field_names = ["id", "Nome", "Distancia ate a cidade"]
+    for i in route:
+        x.add_row([i.ident, i.name, i.g])
+    #print(x)
+    traject.write(str(x))
+    traject.close()
 
 def main():
     file = open("australia.csv", "r")
@@ -95,15 +164,15 @@ def main():
     neighbors = verify_neighbors(all_cities)
 
     #APAGAR
-    print("DADOS DAS CIDADES")
-    for i in range(len(all_cities)):
-        print("nome:" "{0:<20}".format(all_cities[i].name),"id:" "{0:<3}".format(all_cities[i].ident),"vizinhos:", neighbors[i])
+    # print("DADOS DAS CIDADES")
+    # for i in range(len(all_cities)):
+    #     print("nome:" "{0:<20}".format(all_cities[i].name),"id:" "{0:<3}".format(all_cities[i].ident),"vizinhos:", neighbors[i])
 
     #localizar a cidade de destino (Yulara)
     destiny = [find_city(all_cities, "Yulara")]
     #APAGAR
-    print("\nDADOS DESTINO")
-    print("id: ",destiny[0].ident,"x: ", destiny[0].x,"y: ", destiny[0].y)
+    # print("\nDADOS DESTINO")
+    # print("id: ",destiny[0].ident,"x: ", destiny[0].x,"y: ", destiny[0].y)
 
     # Insere a primeira cidade (Alice Springs) no vetor e preenche seus atributos
     cities = [find_city(all_cities, "Alice Springs")]
@@ -114,38 +183,48 @@ def main():
     cities[0].f = cities[0].g + cities[0].h
 
     #APAGAR
-    print("\nDADOS ORIGEM")
-    print("id: ",cities[0].ident,"x: ", cities[0].x,"y: ", cities[0].y)
+    # print("\nDADOS ORIGEM")
+    # print("id: ",cities[0].ident,"x: ", cities[0].x,"y: ", cities[0].y)
 
     # A cidade de origem aponta para posicao 0 do vetor cities
     actual_city = 0
+    # Algoritmo A*
+    while (cities[actual_city].ident != destiny[0].ident):
+        actual_city = a_estrela(cities, all_cities, neighbors, actual_city, destiny)
+
+    # for i in cities:
+    #     print("i:", i.ident, "name:" "{0:<20}".format(i.name),"expand:", i.expand, "pai:", i.pai)
+    # print(actual_city, "name", cities[actual_city].name, "f", cities[actual_city].f)
+
+    route = find_route(cities, actual_city)
+
+    print_route (route, "route_a_estrela.txt")
+
+
+    # Insere a primeira cidade (Alice Springs) no vetor e preenche seus atributos
+    cities = [find_city(all_cities, "Alice Springs")]
+    cities[0].pai = -1
+    cities[0].expand = 0
+    cities[0].g = 0
+    cities[0].h = h_distance(cities[0], destiny[0])
+    cities[0].f = cities[0].g + cities[0].h
+    actual_city = 0
+
     cont = 0
-    #FAZER O LOOP AQUI
-    while (cities[actual_city].ident != destiny[0].ident and cont < 10):
-        #marcar cidade como expandida
-        print("-----------------------------------------------------")
-        cities[actual_city].expand = 1
+    while (cities[actual_city].ident != destiny[0].ident):
 
-        #APAGAR
-        print("\nexpandindo ", cities[actual_city].name, "expand", cities[actual_city].expand)
-
-        # Inserir todos os vizinhos da actual_city no vetor cities
-        insert_neighbors(cities, all_cities, neighbors, actual_city, destiny)
-        n = len(cities)
-
-
-        aux = actual_city
-        for j in range(n):
-            if cities[j].expand == 0:
-                if (cities[j].f < cities[aux].f or cities[aux].expand == 1) and cities[j].ident != cities[0].ident:
-                    if all_cities[int(cities[j].ident)-1].expand == 0:
-                        aux = j
-        actual_city = aux
-        for i in cities:
-             print("i:", i.ident, "name:" "{0:<20}".format(i.name), "f:", i.f,"g:", i.g,"h:", i.h, "expand:", i.expand )
+        actual_city = greedy(cities, all_cities, neighbors, actual_city, destiny)
         cont = cont + 1
-        print(actual_city, "name", cities[actual_city].name, "f", cities[actual_city].f)
-    ##ENCERRA O LOOP AQUI
+
+
+    # for i in cities:
+    #     print("i:", i.ident, "name:" "{0:<20}".format(i.name),"expand:", i.expand, "pai:", i.pai)
+    # print(actual_city, "name", cities[actual_city].name, "f", cities[actual_city].f)
+
+
+    route = find_route(cities, actual_city)
+
+    print_route (route, "route_greedy.txt")
 
     file.close()
 
